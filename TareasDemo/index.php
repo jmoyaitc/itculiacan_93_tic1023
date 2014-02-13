1066fe7875bfa54d5c0dd8bfce7c91bb5d7fadaf
Jorge Moya <moyajg@geminisomega.com>
2014-02-13 16:37:40 -0700

    * Agregamos una lista en HTML utilizando ul.

--- TareasDemo/index.php
+++ TareasDemo/index.php
@@ -3,9 +3,14 @@
 ?><DOCTYPE html>
 <html>
 <head>
-<title>Mi primera paguina web.</title>
+<title>Cosas que tengo que hacer el fin de semana.</title>
 </head>
 <body>
-<h1>Un encabezado</h1>
+<h1>Tareas a realizar</h1>
+<ul>
+	<li>Finalizar este demo de paguina de inicio </li>
+	<li>Terminar de configurar el servidor de la clase </li>
+	<li>Revisar todas las tareas de mis Alumnos </li>
+<ul>
 </body>
 </html>
