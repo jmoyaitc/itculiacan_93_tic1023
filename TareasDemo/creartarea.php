9edc94854dd36b8aec27292676862931f456e4ca
Jorge Moya <moyajg@geminisomega.com>
2014-02-13 17:03:32 -0700

    * Se agrego un separador al menu y saltos de paguina para que se vieran mas bonito.

--- TareasDemo/creartarea.php
+++ TareasDemo/creartarea.php
@@ -8,9 +8,11 @@
 <body>
 <h1>Nueva Tarea</h1>
 <nav><a href="creartarea.php">Mis Tareas</a><nav>
+<!-- Un separador  -->
+<hr />
 <form method="POST">
 <label for="descripcion">Tarea : </label>
-<input type="text" value="" name="descripcion" />
+<input type="text" value="" name="descripcion" /> <br />
 <label for="prioridad">Prioridad : </label>
 <select name="prioridad">
 	<option>1</option>
@@ -18,7 +20,7 @@
 	<option>3</option>
 	<option>4</option>
 </select>
-
+<br />
 <input type="submit" value="Crear"/>
 </form>
 </body>
