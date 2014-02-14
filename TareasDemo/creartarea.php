6de395dd833d71e83f66a0e52606bc2196c56714
Jorge Moya <moyajg@geminisomega.com>
2014-02-13 17:01:45 -0700

    Crear tarea html. * Tiene un Form * dentro del form estan los diferentes campos para ser capturados asi como sus etiquetas. * UN boton de submit.

---
+++ TareasDemo/creartarea.php
@@ -0,0 +1,25 @@
+<?php
+  // Nada de codigo aun. solo el html.
+?><DOCTYPE html>
+<html>
+<head>
+<title>Crear nueva tarea</title>
+</head>
+<body>
+<h1>Nueva Tarea</h1>
+<nav><a href="creartarea.php">Mis Tareas</a><nav>
+<form method="POST">
+<label for="descripcion">Tarea : </label>
+<input type="text" value="" name="descripcion" />
+<label for="prioridad">Prioridad : </label>
+<select name="prioridad">
+	<option>1</option>
+	<option>2</option>
+	<option>3</option>
+	<option>4</option>
+</select>
+
+<input type="submit" value="Crear"/>
+</form>
+</body>
+</html>
