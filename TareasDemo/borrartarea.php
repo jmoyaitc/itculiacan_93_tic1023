288770d6288bf04ae2949e104026b6bea4244c46
Jorge Moya <moyajg@geminisomega.com>
2014-02-13 17:41:03 -0700

    * Borrar tarea agregado. * Enlace para borrar tarea. * Se copio de las otras hojas, se utilizo una lista de definicion dl para mostrar los datos de la tarea.

---
+++ TareasDemo/borrartarea.php
@@ -0,0 +1,28 @@
+<?php
+  // Nada de codigo aun. solo el html.
+?><DOCTYPE html>
+<html>
+<head>
+<title>Borrar tarea XYZ</title>
+<LINK href="site.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Borrar Tarea XYZ</h1>
+<nav><a href="index.php">Mis Tareas</a><nav>
+<!-- Un separador  -->
+<hr />
+<form method="POST">
+<h2>Seguro que desea hacer esto? </h2>
+<dl>
+<dt>
+	Tarea :
+<dt>
+<dd>Implementar borrar una tarea<dd>
+<dt>Prioridad : </dt>
+<dd>3<dd>
+</dl>
+<br />
+<input type="submit" value="Borrar"/>
+</form>
+</body>
+</html>
