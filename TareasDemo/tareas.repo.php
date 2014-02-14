8f0c88af50d401417f0f1530e2174b25def52b5a
Jorge Moya <moyajg@geminisomega.com>
2014-02-13 19:27:17 -0700

    * Repositorio de tareas.repo.php * Este archivo contiene todas las funciones que utilizaremos para manipular tareas. * El resto de los archivos haran referencia a este archivo.

---
+++ TareasDemo/tareas.repo.php
@@ -0,0 +1,32 @@
+<?php
+// Este archivo se engarga de leer de la base de datos o de donde sea nescesario la informacion de las tareas.
+// Este patron de diseño se llama repositorio, es simplemente generar una clase o libreria que se encarge de sacar todos los datos
+// De Tareas. 
+
+//En este caso haremos funciones 
+// http://www.php.net/manual/en/functions.user-defined.php
+function MisTareas( ) {
+	// Debe de regresar una lista de todas mis tareas. 
+	return null;
+}
+
+function LeerTarea($id) {
+	//Debe de regresar una tarea especifica.
+	return null;
+}
+
+function CrearTarea($tarea) {
+	// Debe de crear una tarea. 
+	return null;
+}
+
+function ActualizarTarea($tarea) {
+	//Debe de borrar una tarea si existe.
+	return null;
+}
+
+function BorrarTarea($tarea) {
+	//Debe de borrar la tarea si existe.
+}
+
+?>
