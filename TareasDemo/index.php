d21abdd14266800fa3cf2adfc22065e978f19609
Jorge Moya <moyajg@geminisomega.com>
2014-02-13 19:46:26 -0700

    * Index ahora hace referencia al repositorio de tareas. * Index llama a buscar las tareas actuales * Index genera las tareas en base a la informacion del repositorio. * Esta es ahora en efecto una paguina dinamica.

--- TareasDemo/index.php
+++ TareasDemo/index.php
@@ -1,4 +1,7 @@
 <?php
+require_once("tareas.repo.php");
+
+$mistareas = MisTareas(); //Llamar a la funcion del repositorio. a esta paguina no le importa de donde o como salen las tareas. 
   // Nada de codigo aun. solo el html.
 ?><DOCTYPE html>
 <html>
@@ -11,10 +14,18 @@
 <h1>Tareas a realizar</h1>
 <nav><a href="creartarea.php">Agregar Tarea</a><nav>
 <ul>
+<!--- SI esto parece extraño  < ? php asi de la nada ver esta referencia http://www.php.net/manual/es/language.basic-syntax.phpmode.php -->
+<?php
+	//http://www.php.net/manual/es/control-structures.foreach.php se pueden utilizar muchos opciones aqui for while do . 
+	// Esto es un iterador http://en.wikipedia.org/wiki/Iterator
+	foreach($mistareas as $t) {
+		?>
+		<li><?php echo $t["descripcion"]; ?></li>
+	<?php
+	}
+	?>
 	<li>Finalizar este demo de paguina de inicio <a href="editartarea.php">[Editar ] </a> <a href="borrartarea.php">[Borrar] </a>
 	<a href="comentartarea.php">[Comentar ] </a></li>
-	<li class="completado" >Terminar de configurar el servidor de la clase </li>
-	<li>Revisar todas las tareas de mis Alumnos </li>
 <ul>
 </body>
 </html>
