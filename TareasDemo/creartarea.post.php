390250c745c979c23a303c9d78b4c354884b35b5
Jorge Moya <moyajg@geminisomega.com>
2014-02-18 13:52:39 -0700

    Crear tarea post. * Ahora si implementamos para guardar la tarea utilizando nuestras funciones.

--- TareasDemo/creartarea.post.php
+++ TareasDemo/creartarea.post.php
@@ -1,4 +1,7 @@
 <?php
+//Utilizaremos nuestras funciones para guardar la tarea.
+
+require_once("tareas.repo.php");
   // este archivo solo guarda los datos de una nueva tarea. No debe de regresar nada de html, a menos que exista algun error.
   // para este efecto solo utilizaremos un redirigir. que en php se hace de la siguiente manera.
   // http://php.net/manual/en/function.header.php  
@@ -9,8 +12,17 @@
   $desc = $_POST["descripcion"];
   $prio = $_POST["prioridad"];
   
-  echo $desc . "<br />";
-  echo $prio . "<br />";
+  $tarea = array( "descripcion" => $desc,
+					"prioridad" => $prio
+					);
   
-  //header('Location: index.php');
-  //exit;
+  
+  if(CrearTarea($tarea)) {
+	header('Location: index.php');
+  }else {
+	// Si no pudimos crear la tarea nos regresamos al formulario de crear tarea. 
+	header('Location: creartarea.php');
+  }
+  
+  //tenemos que acabar la ejecucion del script. 
+  exit;
